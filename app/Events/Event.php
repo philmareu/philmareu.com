1a3f7c33a77c5a3f4bfa0c514b652632c4d4b733
Phil Martinez <phil@philsquare.com>
2016-03-25 15:29:34 -0700

    initial commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PhilMareu\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.less('styles.less', 'public/css/styles.css')
+        .scripts([
+            'vendor/jquery-1.11.3.min.js',
+            'vendor/uikit-2.25.0/uikit.js',
+            'scripts.js',
+        ], 'public/js/scripts.js')
+        .version(['public/css/styles.css', 'public/js/scripts.js'])
+});
