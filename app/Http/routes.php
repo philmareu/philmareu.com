399274188ffd153093e08734ea7ea33e7d99d239
Phil Martinez <phil@philsquare.com>
2016-05-17 16:59:52 -0700

    setup posts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,5 +5,7 @@ Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 Route::post('contact', 'PagesController@postContact');
 
+Route::resource('blog', 'PostsController', ['only' => ['index', 'show']]);
+
 Route::get('work/{slug}', 'WorksController@show');
 //Route::get('work', 'WorksController@index');
---
+++ app/Models/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhilMareu\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+}
