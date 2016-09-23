914069449ae8963e832c8ae0d5b07a8ea6231a04
Phil Martinez <phil@philsquare.com>
2016-09-23 10:41:45 -0500

    exp. blog header

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,4 +1,9 @@
-<div class="uk-width-1-3">
-    <h3 class="uk-margin-bottom-remove"><a href="{{ url('blog/' . $post->posted_at->format('Y/m/d') . '/' . $post->slug) }}">{{ $post->title }}</a></h3>
-    <p class="uk-text-muted uk-margin-top-remove">{{ $post->posted_at->format('M jS, Y') }}</p>
+<div class="uk-width-medium-1-3">
+    <div class="uk-panel uk-panel-box">
+        <h3 class="uk-panel-teaser">
+            <pre><code>$pdgaApi->getCourses();</code></pre>
+        </h3>
+        <h3 class="uk-margin-bottom-remove"><a href="{{ url('blog/' . $post->posted_at->format('Y/m/d') . '/' . $post->slug) }}">{{ $post->title }}</a></h3>
+        <p class="uk-text-muted uk-margin-top-remove">{{ $post->posted_at->format('M jS, Y') }}</p>
+    </div>
 </div>
