f766df4ae794fe149ae36982d62c4325989b9198
Phil Martinez <phil@philsquare.com>
2016-10-04 14:09:53 -0500

    jpg phil image

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -17,7 +17,7 @@
                 <div><img src="{{ asset('img/logo-small-white.png') }}" alt="Phil Mareu Logo" width="50" class="uk-hidden-small"><img src="{{ asset('img/logo-small-grey.png') }}" alt="Phil Mareu Logo" width="50" class="uk-visible-small"></div>
                 <h1>Phil Mareu</h1>
                 <p>
-                    <img src="{{ asset('img/pic.png') }}" alt="Phil Mareu Logo" width="100" class="uk-border-circle uk-align-right">
+                    <img src="{{ asset('img/pic.jpg') }}" alt="Phil Mareu Logo" width="100" class="uk-border-circle uk-align-right">
                     Hi, I’m Phil Mareu, a professional web developer and designer with almost 10 years of experience. I strive to build the best UI/UX for web applications and drive it with sensible coding practices and techniques. When I’m not building web things you can find me playing disc golf, hiking or enjoying a tasty beverage while BBQing.</p>
                 <p>
                     <a href="http://twitter.com/philmareu"><i class="uk-icon-twitter uk-icon-button"></i></a>
