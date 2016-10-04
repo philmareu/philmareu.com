3b2c8119e9cdf0e4d0b1be5e6e8c6f6137433456
Phil Martinez <phil@philsquare.com>
2016-10-04 11:12:36 -0500

    small updates to layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,9 +13,8 @@
         <img src="{{ asset('img/header-about.jpg') }}" alt="Blog header image" class="uk-hidden-small">
         <figcaption class="uk-overlay-panel uk-flex uk-flex-middle">
             <div class="uk-container uk-container-center">
-{{--                <img src="{{ asset('img/pic.png') }}" alt="Phil Mareu Logo" width="100" class="uk-border-circle">--}}
                 <h1>About Phil Mareu</h1>
-                <p>Hi, I’m Phil Mareu. I am a professional web designer and developer with almost 10 years of experience. I strive to build the best UI/UX for web applications and drive it with sensible coding practices and techniques. When I’m not building web things you can find me playing disc golf, hiking, or enjoying a tasty beverage while BBQing.</p>
+                <p>{{ $page->description }}</p>
             </div>
         </figcaption>
     </figure>
