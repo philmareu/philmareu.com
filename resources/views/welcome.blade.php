36605a5e096ffeecede84527cf94cfd75bcbd2f6
Phil Mareu <philmareu@gmail.com>
2018-03-20 11:07:58 -0500

    home header concept

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,36 +2,38 @@
 
 @section('content')
 
-    <nav class="uk-navbar-container" uk-navbar>
-        <div class="uk-navbar-left">
-            <ul class="uk-navbar-nav">
-                <li><a href="">Home</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Videos</a></li>
-                <li><a href="">Posts</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Hire Me</a></li>
-            </ul>
-        </div>
-    </nav>
+    <header>
+        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+            <div class="uk-navbar-left">
+                <ul class="uk-navbar-nav">
+                    <li><a href=""><span uk-icon="icon: star; ratio: 1.5;" class="uk-margin-small-right"></span>Phil Mareu</a></li>
+                </ul>
+            </div>
 
-    <header class="uk-text-center uk-section uk-section-default">
-        <h1>Phil Mareu</h1>
+            <div class="uk-navbar-center">
+                <ul class="uk-navbar-nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">Posts</a></li>
+                    <li><a href="">Work</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </div>
+
+            <div class="uk-navbar-right">
+                <div class="uk-navbar-item">
+                    <a href="" class="uk-button uk-button-primary uk-button-small">Hire Me</a>
+                </div>
+            </div>
+        </nav>
+
+        <section class="uk-section uk-section-default uk-text-center uk-margin-large-bottom">
+            <div class="uk-heading-hero">Code & Design</div>
+            <p class="uk-text-large">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa maxime odit possimus quod. Cupiditate debitis deserunt doloribus et ipsam laudantium libero minima molestiae odit praesentium quasi, repellat similique tenetur!</p>
+        </section>
+
     </header>
 
-    <section class="uk-section uk-section-muted">
-        <div class="uk-container">
-            <h2>Recent Videos</h2>
-        </div>
-    </section>
-
-    <section class="uk-section uk-section-default">
-        <div class="uk-container">
-            <h2>Recent Posts</h2>
-        </div>
-    </section>
-
-    <footer class="uk-section uk-section-primary">
-        Phil Mareu
-    </footer>
+    <hr>
 @endsection
