a4b0d24e0bd2a6aea54eacb97b157d642bac3dde
Phil Mareu <philmareu@gmail.com>
2018-03-20 16:28:26 -0500

    setup screencast page concept

---
+++ resources/views/partials/cards/screencast.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="uk-card uk-card-default uk-margin">
+        <div class="uk-card-media-top uk-cover-container">
+            <iframe src="https://www.youtube.com/embed/4p85lZjb8uo" width="560" height="315" frameborder="0" allowfullscreen uk-responsive></iframe>
+        </div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">Adding a new activity feed item</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            <div>
+                <a href="" class="uk-button uk-button-default uk-button-small">View Project</a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/screencasts/index.blade.php
+++ resources/views/screencasts/index.blade.php
@@ -10,4 +10,10 @@
 
 @section('content')
     @include('partials.headers.default')
+
+    <div class="uk-container">
+        <div class="uk-child-width-1-3@s" uk-grid>
+            @each('partials.cards.screencast', [[], [], []], 'screencast')
+        </div>
+    </div>
 @endsection
