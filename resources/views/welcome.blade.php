37ab820e4822ee9b689860d2cec4f44d54ab5c5c
Phil Mareu <philmareu@gmail.com>
2018-03-20 11:25:51 -0500

    updated header concept

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,11 +29,23 @@
         </nav>
 
         <section class="uk-section uk-section-default uk-text-center uk-margin-large-bottom">
-            <div class="uk-heading-hero">Code & Design</div>
-            <p class="uk-text-large">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa maxime odit possimus quod. Cupiditate debitis deserunt doloribus et ipsam laudantium libero minima molestiae odit praesentium quasi, repellat similique tenetur!</p>
+            <div class="uk-container">
+                <div class="uk-heading-hero">Code & Design</div>
+                <p class="uk-text-large">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa maxime odit possimus quod. Cupiditate debitis deserunt doloribus et ipsam laudantium libero minima molestiae odit praesentium quasi, repellat similique tenetur!</p>
+                <div class="uk-text-center">
+                    <a href=""><span uk-icon="icon: twitter;" class="uk-margin-small-right"></span></a>
+                    <a href=""><span uk-icon="icon: github;" class="uk-margin-small-right"></span></a>
+                    <a href=""><span uk-icon="icon: dribbble;" class="uk-margin-small-right"></span></a>
+                    <a href=""><span uk-icon="icon: behance;" class="uk-margin-small-right"></span></a>
+                    <a href=""><span uk-icon="icon: linkedin;" class="uk-margin-small-right"></span></a>
+                    <a href=""><span uk-icon="icon: mail;" class="uk-margin-small-right"></span></a>
+                </div>
+            </div>
         </section>
 
     </header>
 
     <hr>
+
+    <h2>Recent </h2>
 @endsection
