d3644e0d6bfa37e2a5b78bb453fcfd199173dff5
Phil Mareu <philmareu@gmail.com>
2018-03-21 15:58:34 -0500

    gave screencasts a show page with concept layout

--- resources/views/partials/cards/screencast.blade.php
+++ resources/views/partials/cards/screencast.blade.php
@@ -1,13 +1,14 @@
 <div>
     <div class="uk-card uk-card-default uk-margin">
         <div class="uk-card-media-top uk-cover-container">
-            <iframe src="https://www.youtube.com/embed/4p85lZjb8uo" width="560" height="315" frameborder="0" allowfullscreen uk-responsive></iframe>
+            <img src="https://picsum.photos/600" alt="" uk-cover>
+            <canvas width="600" height="400"></canvas>
         </div>
         <div class="uk-card-body">
             <h3 class="uk-card-title">Adding a new activity feed item</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
             <div>
-                <a href="" class="uk-button uk-button-default uk-button-small">View Project</a>
+                <a href="{{ url('screencasts/screencast') }}" class="uk-button uk-button-default uk-button-small">Watch</a>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ Route::view('/', 'welcome');
 Route::view('projects', 'projects.index');
 Route::view('projects/project', 'projects.show');
 Route::view('screencasts', 'screencasts.index');
+Route::view('screencasts/screencast', 'screencasts.show');
 Route::view('work', 'work.index');
 Route::view('posts', 'posts.index');
 Route::view('posts/post', 'posts.show');
