ff3a0c976a33b50d6cfa9b1932a74269fbedd742
Phil Mareu <philmareu@gmail.com>
2018-03-21 10:45:16 -0500

    simple about page concept

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,4 +10,21 @@
 
 @section('content')
     @include('partials.headers.default')
+
+    <div class="uk-container">
+        <div class="uk-card uk-card-default uk-card-small">
+            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+                <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
+                    <img src="https://picsum.photos/400/600" alt="" uk-cover>
+                    <canvas width="600" height="400"></canvas>
+                </div>
+                <div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Phil Mareu</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
