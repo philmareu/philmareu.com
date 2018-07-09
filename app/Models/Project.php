e6eff074597c8bdef0b86a3efeea069e64cd26b0
Phil Mareu <philmareu@gmail.com>
2018-07-09 14:35:07 -0500

    added project resource with some basic fields

---
+++ app/Models/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PhilMareu\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Philsquare\LaraManager\Models\LaramanagerImage;
+
+class Project extends Model
+{
+    public function image()
+    {
+        return $this->belongsTo(LaramanagerImage::class);
+    }
+}
