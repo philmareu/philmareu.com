b9c077a7db5b16cc226998a0d9437d353e4f5ba4
Phil Mareu <philmareu@gmail.com>
2018-07-17 11:46:56 -0500

    removed unused pages for now

--- resources/views/partials/navigation/primary.blade.php
+++ resources/views/partials/navigation/primary.blade.php
@@ -8,16 +8,14 @@
     <div class="uk-navbar-center">
         <ul class="uk-navbar-nav">
             <li><a href="{{ url('/') }}">Home</a></li>
-            <li><a href="{{ url('projects') }}">Projects</a></li>
             <li><a href="{{ url('posts') }}">Posts</a></li>
-            <li><a href="{{ url('work') }}">Work</a></li>
             <li><a href="{{ url('about') }}">About</a></li>
         </ul>
     </div>
 
     <div class="uk-navbar-right">
         <div class="uk-navbar-item">
-            <a href="{{ url('hire') }}" class="uk-button uk-button-primary uk-button-small">Hire Me</a>
+            {{--<a href="{{ url('hire') }}" class="uk-button uk-button-primary uk-button-small">Hire Me</a>--}}
         </div>
     </div>
 </nav>
