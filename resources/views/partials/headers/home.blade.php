6fff1fa29ed37d6fe7759b60e8eca7b11457490b
Phil Mareu <philmareu@gmail.com>
2018-07-18 11:14:52 -0500

    update homepage content

--- resources/views/partials/headers/home.blade.php
+++ resources/views/partials/headers/home.blade.php
@@ -3,15 +3,15 @@
 
     <section class="uk-section uk-section-default uk-text-center uk-margin-large-bottom">
         <div class="uk-container">
-            <div class="uk-heading-hero">Code & Design</div>
+            <div class="uk-heading-hero">Coder & Designer</div>
             <p class="uk-text-large">@yield('description')</p>
             <div class="uk-text-center">
-                <a href=""><span uk-icon="icon: twitter;" class="uk-margin-small-right"></span></a>
-                <a href=""><span uk-icon="icon: github;" class="uk-margin-small-right"></span></a>
-                <a href=""><span uk-icon="icon: dribbble;" class="uk-margin-small-right"></span></a>
-                <a href=""><span uk-icon="icon: behance;" class="uk-margin-small-right"></span></a>
-                <a href=""><span uk-icon="icon: linkedin;" class="uk-margin-small-right"></span></a>
-                <a href=""><span uk-icon="icon: mail;" class="uk-margin-small-right"></span></a>
+                <a href="http://github.com/philmareu"><span uk-icon="icon: github;" class="uk-margin-small-right"></span></a>
+                <a href="http://twitter.com/philmareu"><span uk-icon="icon: twitter;" class="uk-margin-small-right"></span></a>
+                <a href="http://dribbble.com/philmareu"><span uk-icon="icon: dribbble;" class="uk-margin-small-right"></span></a>
+                <a href="https://www.behance.net/philmareud1d9"><span uk-icon="icon: behance;" class="uk-margin-small-right"></span></a>
+                <a href="https://www.linkedin.com/in/philmareu"><span uk-icon="icon: linkedin;" class="uk-margin-small-right"></span></a>
+                <a href="mailto:philmareu@gmail.com"><span uk-icon="icon: mail;" class="uk-margin-small-right"></span></a>
             </div>
         </div>
     </section>
