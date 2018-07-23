f91873100f4dcc77b568930021dfb5419fd97b82
Phil Mareu <philmareu@gmail.com>
2018-07-23 18:37:03 -0500

    switched post to use polymorphic method

--- database/migrations/2018_07_23_233443_alter_posts_table_add_postable_polymorph_fields.php
+++ database/migrations/2018_07_23_233443_alter_posts_table_add_postable_polymorph_fields.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterPostsTableAddBlogSeriesRelation extends Migration
+class AlterPostsTableAddPostablePolymorphFields extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AlterPostsTableAddBlogSeriesRelation extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedInteger('blog_series_id')->nullable();
-            $table->foreign('blog_series_id')->references('id')->on('blog_series')->onUpdate('cascade')->onDelete('set null');
+            $table->morphs('postable');
         });
     }
 
@@ -27,8 +26,7 @@ class AlterPostsTableAddBlogSeriesRelation extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropForeign('posts_blog_series_id_foreign');
-            $table->dropColumn('blog_series_id');
+            $table->dropMorphs('postable');
         });
     }
 }
