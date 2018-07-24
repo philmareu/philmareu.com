79281e0b3cb2f25d4e7b171b6a3472c0c0d5af71
Phil Mareu <philmareu@gmail.com>
2018-07-24 10:21:50 -0500

    updated project show layout

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -24,22 +24,13 @@
     @include('partials.headers.default')
 
     <div class="uk-container">
-        <div class="uk-grid-divider" uk-grid>
-            <div class="uk-width-2-3@s">
+        <div class="uk-child-width-1-2@s uk-grid-divider" uk-grid>
+            <div>
+                <h2>Posts</h2>
                 @each('blog.list.basic', $project->posts, 'post')
             </div>
-            <div class="uk-width-1-3@s">
-                <div uk-margin>
-                    <span class="uk-badge">Laravel/PHP</span>
-                    <span class="uk-badge">Mapbox</span>
-                    <span class="uk-badge">Stripe</span>
-                    <span class="uk-badge">Darksky API</span>
-                    <span class="uk-badge">Laravel/PHP</span>
-                    <span class="uk-badge">Laravel/PHP</span>
-                    <span class="uk-badge">Laravel/PHP</span>
-                    <span class="uk-badge">Laravel/PHP</span>
-                </div>
-                <hr>
+            <div>
+                <h2>Screenshots</h2>
                 <div class="uk-child-width-1-2@s" uk-grid>
                     <div><img src="https://picsum.photos/300" alt=""></div>
                     <div><img src="https://picsum.photos/300" alt=""></div>
