62de934e0a18e4fc351eddbd5372d2fff09ed41a
Phil Mareu <philmareu@gmail.com>
2018-07-23 23:46:27 -0500

    use project Post model

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,15 @@
 namespace PhilMareu\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhilMareu\LaraManagerBlog\Repositories\PostsRepository;
+use PhilMareu\Models\Post;
 
 class BlogController extends \PhilMareu\LaraManagerBlog\Http\Controllers\BlogController
 {
-    //
+    public function __construct(PostsRepository $postsRepository)
+    {
+        $postsRepository = new PostsRepository(new Post);
+
+        parent::__construct($postsRepository);
+    }
 }
