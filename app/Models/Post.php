39fd69ca3edb9c2efae098dd20aa40ec140faa3e
Phil Mareu <philmareu@gmail.com>
2018-07-23 23:16:29 -0500

    added project relation to posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,5 +5,8 @@ namespace PhilMareu\Models;
 
 class Post extends \PhilMareu\LaraManagerBlog\Models\Post
 {
-    
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
