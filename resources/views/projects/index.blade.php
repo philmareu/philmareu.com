5713c5eca2f5d13282830ca611cda56cc5d433e9
Phil Mareu <philmareu@gmail.com>
2018-08-03 09:27:46 -0500

    mock up of layout updates

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -14,8 +14,26 @@ Projects
 
 @section('default-content')
     <div class="uk-container">
-        <div class="uk-child-width-1-2@s" uk-grid>
-            @each('projects.card', $projects, 'project')
-        </div>
+        @foreach($projects as $project)
+            <div class="uk-margin-large" uk-grid>
+                <div class="uk-width-1-3">
+                    <img src="{{ url('images/original/' . $project->image->filename) }}" alt="">
+                </div>
+                <div class="uk-width-2-3">
+                    <h3>{{ $project->title }}</h3>
+                    <p>{{ $project->description }}</p>
+                    <div class="uk-margin">
+                        <a href="{{ url('projects/' . $project->slug) }}" class="uk-button uk-button-default uk-button-small">Details</a>
+                    </div>
+                    <div>
+                        <span class="uk-badge">Laravel</span>
+                        <span class="uk-badge">Vue</span>
+                        <span class="uk-badge">Stripe</span>
+                        <span class="uk-badge">Mapbox</span>
+                        <span class="uk-badge">Algolia</span>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
