fbd752205c157ef971c9cf6da986bd9c7e2d8a2b
Phil Mareu <philmareu@gmail.com>
2024-09-04 13:48:55 -0500

    Build out user factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable implements FilamentUser
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'links' => 'array',
         ];
     }
 
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,12 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'image' => fake()->image(),
+            'logo' => fake()->image(),
+            'hero' => collect(fake()->words())->implode(' '),
+            'summary' => fake()->paragraph(2),
+            'about' => collect(fake()->paragraphs())->implode(' '),
+            'links' => [],
         ];
     }
 
