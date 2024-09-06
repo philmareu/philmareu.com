710a2dfe26140bff4b19efb6794b03c05f3454c7
Phil Mareu <philmareu@gmail.com>
2024-09-06 13:32:21 -0500

    Build out minimal hobbies page

--- resources/views/pages/hobbies/index.blade.php
+++ resources/views/pages/hobbies/index.blade.php
@@ -1,3 +1,26 @@
+<?php
+
+use App\Models\Hobby;
+use function Livewire\Volt\{state};
+
+state([
+    'hobbies' => fn() => Hobby::limit(5)
+        ->get()
+]);
+
+?>
+
 <x-layout>
-    Hobbies
+    <x-header title="Hobbies"/>
+    @volt
+    <section class="w-1/2 mx-auto mt-16 text-center">
+        <div>
+            @foreach($hobbies as $hobby)
+                <div class="mt-4">
+                    <a href="{{ url('hobbies/' . $hobby->slug) }}" class="text-teal-600 text-xl">{{ $hobby->name }}</a>
+                </div>
+            @endforeach
+        </div>
+    </section>
+    @endvolt
 </x-layout>
