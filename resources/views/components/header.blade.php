e19805c7d7d4bf334eac6f0dc87e639c3722daa0
Phil Mareu <philmareu@gmail.com>
2024-09-06 10:55:51 -0500

    Setup minimal posts page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header class="py-16 bg-slate-50">
+    <div class="text-center">
+        <div class="font-serif mt-8">
+            <div class="uppercase text-6xl bg-gradient-to-r from-slate-500 via-slate-400 to-slate-900 text-transparent bg-clip-text inline-block">{{ $title }}</div>
+        </div>
+    </div>
+</header>
--- resources/views/pages/posts/index.blade.php
+++ resources/views/pages/posts/index.blade.php
@@ -11,14 +11,18 @@ state([
 ?>
 
 <x-layout>
-    <h2>Recent Posts</h2>
+    <x-header title="Posts" />
     @volt
-    <div>
-        @foreach($posts as $post)
-            <div>
-                <a href="{{ url('posts/' . $post->slug) }}">{{ $post->name }}</a>
-            </div>
-        @endforeach
-    </div>
+    <section class="w-1/2 mx-auto mt-16 text-center">
+        <div>
+            @foreach($posts as $post)
+                <div class="mt-4">
+                    <a href="{{ url('posts/' . $post->slug) }}" class="text-teal-600 text-xl">{{ $post->name }}</a>
+                    <div>{{ $post->published_at->format('M d, Y') }}</div>
+                    <p class="mt-2">{{ $post->summary }}</p>
+                </div>
+            @endforeach
+        </div>
+    </section>
     @endvolt
 </x-layout>
