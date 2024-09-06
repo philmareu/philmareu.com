1c1ef4cde34cd3a0f28bad94982b820531580eb0
Phil Mareu <philmareu@gmail.com>
2024-09-06 11:05:17 -0500

    Setup posts show page

--- resources/views/pages/posts/[Post:slug].blade.php
+++ resources/views/pages/posts/[Post:slug].blade.php
@@ -1,3 +1,10 @@
-<div>
-    //
-</div>
+<x-layout>
+    <x-header :title="$post->name" />
+
+    <section class="w-1/2 mx-auto mt-16">
+        <div class="mt-4">
+            <div>{{ $post->published_at->format('M d, Y') }}</div>
+            <div class="mt-2 prose">{!! \Illuminate\Support\Str::markdown($post->content) !!}</div>
+        </div>
+    </section>
+</x-layout>
