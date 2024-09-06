5391af0b6e635cb9298a8e9a0fbb629b4e34cea3
Phil Mareu <philmareu@gmail.com>
2024-09-06 13:48:26 -0500

    Setup minimal about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,10 @@
+@php $user = \App\Services\PrimaryUser::retrieve() @endphp
+
 <x-layout>
-    <div>{!! $user->about !!}</div>
-    <div>Timeline</div>
+    <x-header title="About" />
+
+    <section class="w-1/2 mx-auto mt-16">
+        <div class="prose max-w-none">{!! \Illuminate\Support\Str::markdown($user->about) !!}</div>
+        <div>Timeline</div>
+    </section>
 </x-layout>
